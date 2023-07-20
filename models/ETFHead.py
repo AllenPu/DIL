134b7f49cd75e118e8e7ee5793e7a7e388d32b6d
ruizhipu <18651885620@163.com>
2023-07-20 17:21:56 +0800

    finish etf

--- models/ETFHead.py
+++ models/ETFHead.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.distributed as dist
+from models.loss import DRLoss
 
 
 def generate_random_orthogonal_matrix(feat_in, num_classes):
@@ -15,11 +16,26 @@ def generate_random_orthogonal_matrix(feat_in, num_classes):
     return orth_vec
 
 
-class ETFHead():
+class ETFHead(nn.Module):
     def __init__(self, num_classes : int, feat_in : int) -> None:
         orth_vec = generate_random_orthogonal_matrix(feat_in, num_classes)
         i_nc_nc = torch.eye(num_classes)
         one_nc_nc: torch.Tensor = torch.mul(torch.ones(
             num_classes, num_classes), (1 / num_classes))
-        etf_vec = torch.mul(torch.matmul(orth_vec, i_nc_nc - one_nc_nc),
+        self.etf_vec = torch.mul(torch.matmul(orth_vec, i_nc_nc - one_nc_nc),
                             math.sqrt(num_classes / (num_classes - 1)))
+
+
+    def pre_logits(self, x):
+        return x / torch.norm(x, p=2, dim=1, keepdim=True)
+
+
+    def forward(self, x, y):
+        x = self.pre_logits(x)
+        #
+        pred = self.etf_vec[:, y].t()
+        #
+        loss = DRLoss(x, pred)
+        #
+        return loss
+        
