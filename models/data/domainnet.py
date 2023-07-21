15509aff5f93a0e590b51367c30686963ab3dbc5
ruizhipu <18651885620@163.com>
2023-07-21 21:18:39 +0800

    write dataset

---
+++ models/data/domainnet.py
@@ -0,0 +1,55 @@
+import torchvision.transforms as transforms
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import os
+
+
+class iDomainNet(Dataset):
+
+    use_path = True
+    train_trsf = [
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+    ]
+    test_trsf = [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+    ]
+    common_trsf = [
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+
+
+    def __init__(self, args):
+        self.args = args
+        class_order = np.arange(6 * 345).tolist()
+        self.class_order = class_order
+        self.domain_names = ["clipart", "infograph", "painting", "quickdraw", "real", "sketch", ]
+
+    def download_data(self):
+        self.image_list_root = self.args["data_path"]
+
+        image_list_paths = [os.path.join(self.image_list_root, d + "_" + "train" + ".txt") for d in self.domain_names]
+        imgs = []
+        for taskid, image_list_path in enumerate(image_list_paths):
+            image_list = open(image_list_path).readlines()
+            imgs += [(val.split()[0], int(val.split()[1]) + taskid * 345) for val in image_list]
+        train_x, train_y = [], []
+        for item in imgs:
+            train_x.append(os.path.join(self.image_list_root, item[0]))
+            train_y.append(item[1])
+        self.train_data = np.array(train_x)
+        self.train_targets = np.array(train_y)
+
+        image_list_paths = [os.path.join(self.image_list_root, d + "_" + "test" + ".txt") for d in self.domain_names]
+        imgs = []
+        for taskid, image_list_path in enumerate(image_list_paths):
+            image_list = open(image_list_path).readlines()
+            imgs += [(val.split()[0], int(val.split()[1]) + taskid * 345) for val in image_list]
+        train_x, train_y = [], []
+        for item in imgs:
+            train_x.append(os.path.join(self.image_list_root, item[0]))
+            train_y.append(item[1])
+        self.test_data = np.array(train_x)
+        self.test_targets = np.array(train_y)
